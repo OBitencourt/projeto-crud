2902eb50bc7b61c06050b19f8a0d47c12ed5f744
Arthur Bitencourt <arthurbitencourtvsilva@gmail.com>
2024-09-04 21:44:54 +0000

    adding document schema-model

--- src/database/index.js
+++ src/database/index.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose')
 
 function connect() {
 
-    mongoose.connect('mongodb://localhost:27017/')
+    mongoose.connect('mongodb://localhost:27017/projeto-crud')
     
     const db = mongoose.connection
     
---
+++ src/models/costumers.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    email: String,
+    password: String,
+})
+
+const Model = mongoose.model('costumers', schema)
